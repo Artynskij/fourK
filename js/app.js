674b5556d6e534c1088d7f067441661c8f10237a
artem <aartynskij0001@gmail.com>
2023-08-01 16:43:32 +0300

    some problem with swiper

--- js/app.js
+++ js/app.js
@@ -1,40 +1,52 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs";
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: false,
-    
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-    mousewheel: { eventsTarget: 'body', },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // //   draggable: true,
-    // },
-  });
-const swiperStaff = new Swiper('.swiper-staff', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 3,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    // mousewheel: { eventsTarget: 'body', },
-  
-  
-  });
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    // type: 'bullets',
+    clickable: true,
+  },
+  parallax: true,
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  mousewheel: { eventsTarget: "body" },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // //   draggable: true,
+  // },
+});
+const swiperStaff = new Swiper(".swiper-staff", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-staff-button-next",
+    prevEl: ".swiper-staff-button-prev",
+  },
+  // mousewheel: { eventsTarget: 'body', },
+});
+const swiperProduct = new Swiper(".swiper-product-first", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-product-first-button-next",
+    prevEl: ".swiper-product-first-button-prev",
+  },
+  // mousewheel: { eventsTarget: 'body', },
+});
